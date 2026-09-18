eb6d54ac7da712968b498e9cb6ed5164adf70944
joyhong2005 <jhong11@berklee.edu>
2026-09-18 12:25:13 -0400

    Create frog_pond1.js

---
+++ frog_pond1.js
@@ -0,0 +1,9 @@
+let numFrogs = prompt("How many frogs are about to jump in?");
+
+const maxFrogCapacity = 15;
+
+let isPondOverCapacity = numFrogs > maxFrogCapacity;
+
+let messageToPrint = isPondOverCapacity ? "It's too crowded!" : "Come on in!";
+
+print(messageToPrint);
