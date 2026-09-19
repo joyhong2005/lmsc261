b8d90ce553e16ec90eb45acdfd4f10a751988eac
joyhong2005 <jhong11@berklee.edu>
2026-09-19 01:28:35 -0400

    Update frog_pond1.js

--- pset2 /frog_pond1.js
+++ pset2 /frog_pond1.js
@@ -1,9 +1,6 @@
 let numFrogs = prompt("How many frogs are about to jump in?");
-
 const maxFrogCapacity = 15;
-
 let isPondOverCapacity = numFrogs > maxFrogCapacity;
-
-let messageToPrint = isPondOverCapacity ? "It's too crowded!" : "Come on in!";
+let messageToPrint = isPondOverCapacity ? "It's too crowded :((( !" : "jumpppp in!";
 
 print(messageToPrint);
